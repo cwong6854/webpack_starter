9c7adf9323e5ee4f30c3424178b7dd24dd8670fc
Curtis Wong <cwong990@berkeley.edu>
2023-05-01 11:09:00 -0700

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
   devtool: false,
   devServer: {
     static: {
